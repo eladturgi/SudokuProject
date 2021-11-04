3297b3ddd62c8853676e446734425837d70e0e72
gadi <gadi8998@gmail.com>
2021-11-04 20:09:35 +0200

    Cube, Row, Col Validation

--- sudoku.js
+++ sudoku.js
@@ -6,67 +6,74 @@ const mediumLevel = Math.floor(length * length * 0.5);
 const hardLevel = Math.floor(length * length * 0.25);
 generateGameBoardEasyLevel();
 function generateGameBoardEasyLevel() {
-  let mat = [[], [], [], [], [], [], [], [], []];
-
+  let shownMat = [[], [], [], [], [], [], [], [], []];
+  
   let row = 0;
   let col = 0;
   let val = 0;
   //let counter = 0;
   let flag = true;
+
   for (let i = 0; i < easyLevel; i++) {
     flag = true;
     while (flag) {
       row = Math.floor(Math.random() * 9); //0-8
       col = Math.floor(Math.random() * 9); //0-8
       val = Math.floor(Math.random() * 9) + 1; // 1 - 9
-      if (checkSuitability(mat, row, col, val)) {
-        mat[row][col] = val;
+      if ( shownMat[row][col] ==undefined && checkSuitability(shownMat, row, col, val) ) {
+        shownMat[row][col] = val;
         flag = false;
         //counter++;
-        console.log(val + " " + i);
+        console.log(shownMat[row][col] + " " + i);
       }
     }
   }
 
-  printMat(mat);
+  printshownMat(shownMat);
 }
 
-function checkSuitability(mat, row, col, val) {
+function checkSuitability(shownMat, row, col, val) {
   if (
-    checkRow(mat, row, val) &&
-    checkCol(mat, col, val) &&
-    checkCube(mat, row, col, val)
-  )
-    return true;
+    checkRow(shownMat, row, val) &&
+    checkCol(shownMat, col, val) &&
+    checkCube(shownMat, row, col, val)
+  ) return true;
+
   return false;
 }
-function checkRow(mat, row, val) {
+function checkRow(shownMat, row, val) {
   for (let i = 0; i < length; i++) {
-    if (mat[row][i] == val) return false;
+    if (shownMat[row][i] == val) return false;
   }
+
   return true;
 }
-function checkCol(mat, col, val) {
+function checkCol(shownMat, col, val) {
   for (let i = 0; i < length; i++) {
-    if (mat[i][col] == val) return false;
+    if (shownMat[i][col] == val) return false;
   }
+
   return true;
 }
-function checkCube(mat, row, col, val) {
+function checkCube(shownMat, row, col, val) {
   let startCubeRow = row - (row % 3);
-  let startCubeCol = row - (row % 3);
+  let startCubeCol = col - (col % 3);
+
+//   document.write(startCubeCol+" "+startCubeRow)
+//   document.write("</br>")
   for (let i = startCubeRow; i <= startCubeRow + 2; i++) {
     for (let j = startCubeCol; j <= startCubeCol + 2; j++) {
-      if (mat[i][j] == val) return false;
+      if (shownMat[i][j] == val) return false;
     }
   }
+
   return true;
 
   //   if (row >= 0 && row <= 2) {
   //     if (col >= 0 && col <= 2) {
   //       for (let i = 0; i <= 2; i++) {
   //         for (let j = 0; j <= 2; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -74,7 +81,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 3 && col <= 5) {
   //       for (let i = 0; i <= 2; i++) {
   //         for (let j = 3; j <= 5; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -82,7 +89,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 6 && col <= 8) {
   //       for (let i = 0; i <= 2; i++) {
   //         for (let j = 6; j <= 8; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -91,7 +98,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 0 && col <= 2) {
   //       for (let i = 3; i <= 5; i++) {
   //         for (let j = 0; j <= 2; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -99,7 +106,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 3 && col <= 5) {
   //       for (let i = 3; i <= 5; i++) {
   //         for (let j = 3; j <= 5; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -107,7 +114,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 6 && col <= 8) {
   //       for (let i = 3; i <= 5; i++) {
   //         for (let j = 6; j <= 8; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -116,7 +123,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 0 && col <= 2) {
   //       for (let i = 6; i <= 8; i++) {
   //         for (let j = 0; j <= 2; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -124,7 +131,7 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 3 && col <= 5) {
   //       for (let i = 6; i <= 8; i++) {
   //         for (let j = 3; j <= 5; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
@@ -132,18 +139,27 @@ function checkCube(mat, row, col, val) {
   //     if (col >= 6 && col <= 8) {
   //       for (let i = 6; i <= 8; i++) {
   //         for (let j = 6; j <= 8; j++) {
-  //           if (mat[i][j] == val) return false;
+  //           if (shownMat[i][j] == val) return false;
   //         }
   //       }
   //       return true;
   //     }
   //   }
 }
-function printMat(mat) {
+function printshownMat(shownMat) {
+  let counter = 0;
+
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
-      document.write(mat[i][j] + ",   ");
+        if(shownMat[i][j] == undefined) {
+            document.write("_,   ");
+        }
+        else{
+            document.write(shownMat[i][j] + ",   ");
+            counter++;
+        }
     }
     document.write("</br>");
   }
+  document.write(counter);
 }
