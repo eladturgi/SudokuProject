1c76181db4dc39a4f79c0bebb816a1e42db596b9
eladturgi <eladto@ac.sce.ac.il>
2021-11-06 13:22:11 +0200

    Create board on html through JS

--- sudoku.js
+++ sudoku.js
@@ -4,10 +4,10 @@ const easyLevel = Math.floor(length * length * 0.75);
 const mediumLevel = Math.floor(length * length * 0.5);
 
 const hardLevel = Math.floor(length * length * 0.25);
-generateGameBoardEasyLevel();
+//generateGameBoardEasyLevel();
 function generateGameBoardEasyLevel() {
   let shownMat = [[], [], [], [], [], [], [], [], []];
-  
+
   let row = 0;
   let col = 0;
   let val = 0;
@@ -20,7 +20,10 @@ function generateGameBoardEasyLevel() {
       row = Math.floor(Math.random() * 9); //0-8
       col = Math.floor(Math.random() * 9); //0-8
       val = Math.floor(Math.random() * 9) + 1; // 1 - 9
-      if ( shownMat[row][col] ==undefined && checkSuitability(shownMat, row, col, val) ) {
+      if (
+        shownMat[row][col] == undefined &&
+        checkSuitability(shownMat, row, col, val)
+      ) {
         shownMat[row][col] = val;
         flag = false;
         //counter++;
@@ -37,7 +40,8 @@ function checkSuitability(shownMat, row, col, val) {
     checkRow(shownMat, row, val) &&
     checkCol(shownMat, col, val) &&
     checkCube(shownMat, row, col, val)
-  ) return true;
+  )
+    return true;
 
   return false;
 }
@@ -59,8 +63,8 @@ function checkCube(shownMat, row, col, val) {
   let startCubeRow = row - (row % 3);
   let startCubeCol = col - (col % 3);
 
-//   document.write(startCubeCol+" "+startCubeRow)
-//   document.write("</br>")
+  //   document.write(startCubeCol+" "+startCubeRow)
+  //   document.write("</br>")
   for (let i = startCubeRow; i <= startCubeRow + 2; i++) {
     for (let j = startCubeCol; j <= startCubeCol + 2; j++) {
       if (shownMat[i][j] == val) return false;
@@ -68,98 +72,75 @@ function checkCube(shownMat, row, col, val) {
   }
 
   return true;
-
-  //   if (row >= 0 && row <= 2) {
-  //     if (col >= 0 && col <= 2) {
-  //       for (let i = 0; i <= 2; i++) {
-  //         for (let j = 0; j <= 2; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 3 && col <= 5) {
-  //       for (let i = 0; i <= 2; i++) {
-  //         for (let j = 3; j <= 5; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 6 && col <= 8) {
-  //       for (let i = 0; i <= 2; i++) {
-  //         for (let j = 6; j <= 8; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //   } else if (row >= 3 && row <= 5) {
-  //     if (col >= 0 && col <= 2) {
-  //       for (let i = 3; i <= 5; i++) {
-  //         for (let j = 0; j <= 2; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 3 && col <= 5) {
-  //       for (let i = 3; i <= 5; i++) {
-  //         for (let j = 3; j <= 5; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 6 && col <= 8) {
-  //       for (let i = 3; i <= 5; i++) {
-  //         for (let j = 6; j <= 8; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //   } else {
-  //     if (col >= 0 && col <= 2) {
-  //       for (let i = 6; i <= 8; i++) {
-  //         for (let j = 0; j <= 2; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 3 && col <= 5) {
-  //       for (let i = 6; i <= 8; i++) {
-  //         for (let j = 3; j <= 5; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //     if (col >= 6 && col <= 8) {
-  //       for (let i = 6; i <= 8; i++) {
-  //         for (let j = 6; j <= 8; j++) {
-  //           if (shownMat[i][j] == val) return false;
-  //         }
-  //       }
-  //       return true;
-  //     }
-  //   }
 }
 function printshownMat(shownMat) {
   let counter = 0;
 
   for (let i = 0; i < length; i++) {
     for (let j = 0; j < length; j++) {
-        if(shownMat[i][j] == undefined) {
-            document.write("_,   ");
-        }
-        else{
-            document.write(shownMat[i][j] + ",   ");
-            counter++;
-        }
+      if (shownMat[i][j] == "") {
+        document.write("_,   ");
+      } else {
+        document.write(shownMat[i][j] + ",   ");
+        counter++;
+      }
     }
     document.write("</br>");
   }
   document.write(counter);
+}
+function sudokuEasyLevel() {
+  let easyLevelMatrice = [
+    [6, 8, 1, 2, 9, 3, 5, 7, 4],
+    [3, 7, 2, 5, 6, 4, 1, 8, 9],
+    [4, 9, 5, 8, 7, 1, 2, 3, 6],
+    [1, 6, 4, 3, 8, 2, 9, 5, 7],
+    [8, 5, 9, 7, 4, 6, 3, 2, 1],
+    [2, 3, 7, 9, 1, 5, 6, 4, 8],
+    [5, 4, 8, 1, 2, 9, 7, 6, 3],
+    [7, 1, 3, 6, 5, 8, 4, 9, 2],
+    [9, 2, 6, 4, 3, 7, 8, 1, 5],
+  ];
+  let row = 0;
+  let col = 0;
+  let flag = true;
+  //val = Math.floor(Math.random() * 9) + 1; // 1 - 9
+  for (let i = 0; i < 21; i++) {
+    flag = true;
+    while (flag) {
+      row = Math.floor(Math.random() * 9); //0-8
+      col = Math.floor(Math.random() * 9); //0-8
+      if (easyLevelMatrice[row][col] != "") {
+        easyLevelMatrice[row][col] = "";
+        flag = false;
+      }
+    }
+  }
+  printshownMat(easyLevelMatrice);
+  return easyLevelMatrice;
+}
+game(sudokuEasyLevel());
+
+function game(mat) {
+  for (let row = 0; row < length; row++) {
+    let div = document.createElement("div");
+    let strRow = row.toString();
+    div.setAttribute("id", strRow);
+    div.setAttribute("class", "col");
+    document.getElementById("gameboard").appendChild(div);
+    for (let col = 0; col < length; col++) {
+      if (mat[row][col] == "") {
+        let input = document.createElement("input");
+        input.setAttribute("type", "text");
+
+        document.getElementById(strRow).appendChild(input);
+      } else {
+        let output = document.createElement("OUTPUT");
+        output.innerHTML = mat[row][col];
+
+        document.getElementById(strRow).appendChild(output);
+      }
+    }
+    document.write("</br>");
+  }
 }
